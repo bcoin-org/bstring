c9f132e53fda4f60309cb8aa1e8ff8f5f6bd33e3
Braydon Fuller <courier@braydon.com>
2018-06-19 14:12:38 -0700

    cashaddr: add bindings for c implementation

--- src/bstring.cc
+++ src/bstring.cc
@@ -12,6 +12,7 @@
 
 #include "base58.h"
 #include "bech32.h"
+#include "cashaddr.h"
 #include "bstring.h"
 
 NAN_METHOD(base58_encode) {
@@ -164,6 +165,117 @@ NAN_METHOD(bech32_test) {
   info.GetReturnValue().Set(Nan::New<v8::Boolean>(result));
 }
 
+NAN_METHOD(cashaddr_encode) {
+  if (info.Length() < 3)
+    return Nan::ThrowError("cashaddr_encode() requires arguments.");
+
+  if (!info[0]->IsString())
+    return Nan::ThrowError("First argument must be a string.");
+
+  Nan::Utf8String prefix_str(info[0]);
+
+  if (!info[1]->IsNumber())
+    return Nan::ThrowTypeError("Invalid cashaddr type.");
+
+  v8::Local<v8::Object> hashbuf = info[2].As<v8::Object>();
+
+  if (!node::Buffer::HasInstance(hashbuf))
+    return Nan::ThrowTypeError("Third argument must be a buffer.");
+
+  const char *prefix = (const char *)*prefix_str;
+  int32_t type = (int32_t)info[1]->Int32Value();
+
+  const uint8_t *hash = (uint8_t *)node::Buffer::Data(hashbuf);
+  size_t hash_len = node::Buffer::Length(hashbuf);
+
+  char output[197];
+  memset(&output, 0, 197);
+  size_t olen = 0;
+
+  bstring_cashaddr_error err = bstring_cashaddr_ERR_NULL;
+
+  if (!bstring_cashaddr_encode(&err, output, prefix, type, hash, hash_len))
+    return Nan::ThrowError(bstring_cashaddr_strerror(err));
+
+  olen = strlen((char *)output);
+
+  info.GetReturnValue().Set(
+    Nan::New<v8::String>((char *)output, olen).ToLocalChecked());
+}
+
+NAN_METHOD(cashaddr_decode) {
+  if (info.Length() < 3)
+    return Nan::ThrowError("cashaddr_decode() requires arguments.");
+
+  if (!info[0]->IsString())
+    return Nan::ThrowError("First argument must be a string.");
+
+  if (!info[1]->IsString())
+    return Nan::ThrowError("Second argument must be a string.");
+
+  if (!info[2]->IsObject())
+    return Nan::ThrowError("Third argument must be an object.");
+
+  Nan::Utf8String addr_(info[0]);
+  const char *addr = (const char *)*addr_;
+
+  Nan::Utf8String default_prefix_(info[1]);
+  const char *default_prefix = (const char *)*default_prefix_;
+
+  v8::Local<v8::Object> ret = info[2].As<v8::Object>();
+
+  uint8_t hash[65];
+  memset(hash, 0, 65);
+  size_t hash_len;
+  int type;
+  char prefix[84];
+  memset(prefix, 0, 84);
+  size_t prefix_len;
+
+  bstring_cashaddr_error err = bstring_cashaddr_ERR_NULL;
+
+  if (!bstring_cashaddr_decode(&err, &type, hash, &hash_len, prefix, default_prefix, addr))
+    return Nan::ThrowError(bstring_cashaddr_strerror(err));
+
+  prefix_len = strlen((char *)&prefix[0]);
+
+  Nan::Set(ret,
+    Nan::New<v8::String>("prefix").ToLocalChecked(),
+    Nan::New<v8::String>((char *)&prefix[0], prefix_len).ToLocalChecked());
+
+  Nan::Set(ret,
+    Nan::New<v8::String>("type").ToLocalChecked(),
+    Nan::New<v8::Number>(type));
+
+  Nan::Set(ret,
+    Nan::New<v8::String>("hash").ToLocalChecked(),
+    Nan::CopyBuffer((char *)&hash[0], hash_len).ToLocalChecked());
+
+  info.GetReturnValue().Set(ret);
+}
+
+NAN_METHOD(cashaddr_test) {
+  if (info.Length() < 1 || !info[0]->IsString()) {
+    info.GetReturnValue().Set(Nan::New<v8::Boolean>(false));
+    return;
+  }
+
+  if (!info[1]->IsString())
+    return Nan::ThrowError("Second argument must be a string.");
+
+  Nan::Utf8String addr_(info[0]);
+  const char *addr = (const char *)*addr_;
+
+  Nan::Utf8String default_prefix_(info[1]);
+  const char *default_prefix = (const char *)*default_prefix_;
+
+  bstring_cashaddr_error err = bstring_cashaddr_ERR_NULL;
+
+  bool result = bstring_cashaddr_test(&err, default_prefix, addr);
+
+  info.GetReturnValue().Set(Nan::New<v8::Boolean>(result));
+}
+
 NAN_MODULE_INIT(init) {
   Nan::Export(target, "base58_encode", base58_encode);
   Nan::Export(target, "base58_decode", base58_decode);
@@ -171,6 +283,9 @@ NAN_MODULE_INIT(init) {
   Nan::Export(target, "bech32_encode", bech32_encode);
   Nan::Export(target, "bech32_decode", bech32_decode);
   Nan::Export(target, "bech32_test", bech32_test);
+  Nan::Export(target, "cashaddr_encode", cashaddr_encode);
+  Nan::Export(target, "cashaddr_decode", cashaddr_decode);
+  Nan::Export(target, "cashaddr_test", cashaddr_test);
 }
 
 NODE_MODULE(bstring, init)
